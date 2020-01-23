dc8a2d8ea8cc40e4d51c5d003a35fa457e9ba69c
Lauri Hiltunen <lauri.hiltunen@tuni.fi>
2020-01-23 15:55:48 +0200

    Lisatty myos 7:lla jaollisten lukujen kasittely

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -11,9 +11,15 @@ int main()
     while ( luku < maksimi )
     {
             ++luku;
-        if ( luku % 3 == 0 ) {
+        if ( luku % 3 == 0 && luku % 7 == 0) {
+            std::cout << "zip boing" << std::endl;
+        }
+        else if ( luku % 3 == 0 ) {
              std::cout << "zip" << std::endl;
-    }
+        }
+        else if (luku % 7 == 0 ){
+            std::cout << "boing" << std::endl;
+        }
         else {
             std::cout << luku << std::endl;
         }
