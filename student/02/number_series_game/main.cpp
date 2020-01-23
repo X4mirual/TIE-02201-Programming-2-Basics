d53ce39940c3dc851d635a0781b8ecb10c721ee2
Lauri Hiltunen <lauri.hiltunen@tuni.fi>
2020-01-23 15:43:18 +0200

    Syotteen lukeminen ja numeroiden tulostus

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -4,4 +4,20 @@
 int main()
 {
     std::cout << "How many numbers would you like to have? ";
+    int maksimi = 0;
+    std::cin >> maksimi;
+    int luku = 0;
+
+    while ( luku < maksimi )
+    {
+            ++luku;
+            std::cout << luku << std::endl;
+}
+
+
+
+
+
+
+
 }
