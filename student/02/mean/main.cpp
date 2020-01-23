45b97d0be278ef12d987357784e84590e9b44006
Lauri Hiltunen <lauri.hiltunen@tuni.fi>
2020-01-23 19:12:13 +0200

    valmis :)

--- student/02/mean/main.cpp
+++ student/02/mean/main.cpp
@@ -7,9 +7,18 @@ int main()
     std::cout << "From how many integer numbers you want to count the mean value? ";
     int lukujen_lkm = 0;
     std::cin >> lukujen_lkm;
+    float summa = 0.0;
+    float uusi = 0.0;
 
     if (lukujen_lkm > 0) {
-        return 0;
+        for ( int i = 1; i <= lukujen_lkm; ++i) {
+
+            std::cout << "Input " << i << " number: ";
+            std::cin >> uusi;
+            summa = summa + uusi;
+        }
+        float keskiarvo= summa / lukujen_lkm;
+        std::cout << "Mean value of the given numbers is " << keskiarvo << std::endl;
     }
 
     else {
