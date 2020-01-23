9ae88b0e4d7c449c0d5e4d0c969075e712f68871
Lauri Hiltunen <lauri.hiltunen@tuni.fi>
2020-01-23 18:41:34 +0200

    positiivisen luvun tarkastus

--- student/02/mean/main.cpp
+++ student/02/mean/main.cpp
@@ -5,4 +5,25 @@
 int main()
 {
     std::cout << "From how many integer numbers you want to count the mean value? ";
+    int lukujen_lkm = 0;
+    std::cin >> lukujen_lkm;
+
+    if (lukujen_lkm > 0) {
+        return 0;
+    }
+
+    else {
+        std::cout << "Cannot count mean value from " << lukujen_lkm
+                  << " numbers" << std::endl;
+    }
+
+
+
+
+
+
+
+
+
+
 }
