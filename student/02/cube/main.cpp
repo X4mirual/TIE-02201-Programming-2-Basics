c09ccff415049d7cf941d1c87e70e7809b34202e
Lauri Hiltunen <lauri.hiltunen@tuni.fi>
2020-01-24 06:58:41 +0200

    korjattu nollan kasittely

--- student/02/cube/main.cpp
+++ student/02/cube/main.cpp
@@ -7,18 +7,24 @@ int main()
     int annettu_luku;
     cout << "Enter a number: ";
     cin >> annettu_luku;
-    int luku = annettu_luku * annettu_luku
-            * annettu_luku;
+    if (annettu_luku != 0) {
+        int luku = annettu_luku * annettu_luku
+                * annettu_luku;
 
-    if (luku / annettu_luku / annettu_luku
-            == annettu_luku) {
-        cout << "The cube of " << annettu_luku
-             << " is " << luku << "." << endl;
+        if (luku / annettu_luku / annettu_luku
+                == annettu_luku) {
+            cout << "The cube of " << annettu_luku
+                 << " is " << luku << "." << endl;
+        } else {
+            cout << "Error! The cube of "
+                 << annettu_luku << " is not "
+                 << luku << "." << endl;
+
+        }
+    } else {
+        cout << "The cube of 0 is 0." << endl;
     }
 
-    else {
-        cout << "Error! The cube of "
-             << annettu_luku << " is not "
-             << luku << "." << endl;
-    }
+
+
 }
