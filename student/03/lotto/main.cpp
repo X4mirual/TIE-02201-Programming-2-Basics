46eb9ce57abfbe4a51f23afa3c39229074088dd5
Lauri Hiltunen <lauri.hiltunen@tuni.fi>
2020-01-24 11:48:46 +0200

    korjattu että valittavia palloja voi olla enemmän kuin arvottavia yhteensä

--- student/03/lotto/main.cpp
+++ student/03/lotto/main.cpp
@@ -26,12 +26,16 @@ int main() {
     int lp_arvottavienlkm;
     cin >> lp_arvottavienlkm;
 
-    if (lp_kokonaislkm > 0 and lp_arvottavienlkm > 0) {
-        tulostus(lp_arvottavienlkm, lp_kokonaislkm);
-
-    } else {
+    if (lp_arvottavienlkm < 0 or lp_kokonaislkm < 0) {
         cout << "The number of balls must be a positive number."
              << endl;
+    }
+    else if (lp_arvottavienlkm > lp_kokonaislkm) {
+        cout << "The maximum number of drawn balls is the total amount of balls."
+             << endl;
+    } else {
+        tulostus(lp_arvottavienlkm, lp_kokonaislkm);
+
     }
     return EXIT_SUCCESS;
 }
