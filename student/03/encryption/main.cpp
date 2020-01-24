9f132feca453c5010385fca8b2a6e9bb8b58eb98
Lauri Hiltunen <lauri.hiltunen@tuni.fi>
2020-01-24 15:50:21 +0200

    encryption 2. välipalautus

--- student/03/encryption/main.cpp
+++ student/03/encryption/main.cpp
@@ -1,12 +1,46 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+bool tarkistus(string avain) {
+    if (avain.length() != 26) {
+        cout << "Error! The encryption key must contain"
+                " 26 characters." << endl;
+        return false;
+    }
+    string aakkoset = "abcdefghijklmnopqrstuvwxyz";
+    for (int n = 0; n < 26; ++n) {
+        if (islower(avain.at(n)) == false) {
+            cout << "Error! The encryption key must contain only lower "
+                    "case characters." << endl;
+            return false;
+        }
+        else if (avain.find(aakkoset.at(n)) == string::npos) {
+            cout << "Error! The encryption key must contain all "
+                    "alphabets a-z." << endl;
+            return false;
+        }
+    }
+}
+
+string kryptaus(string avain, string teksti){
+    for (int i = 0; i < 26; ++i) {
+        teksti.at(i) = avain.at(i);
+    }
+
+}
 
 
 int main()
 {
-    //return EXIT_FAILURE;
-    cout << "kissa" << endl;
+    string avain;
+    cout << "Enter the encryption key: ";
+    getline(cin, avain);
+    string teksti;
+    cout << "Enter the text to be encrypted: ";
+    getline(cin, teksti);
+
+    tarkistus(avain)
 
 }
