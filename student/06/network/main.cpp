142ef45da82ce20e2965ff6f6c6f1b5dbe9af428
Lauri Hiltunen <lauri.hiltunen@tuni.fi>
2020-02-20 12:50:19 +0200

    ai että

--- student/06/network/main.cpp
+++ student/06/network/main.cpp
@@ -2,10 +2,12 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <utility>
 
 const std::string HELP_TEXT = "S = store id1 i2\nP = print id\n"
                               "C = count id\nD = depth id\n";
 
+using namespace std;
 
 std::vector<std::string> split(const std::string& s, const char delimiter, bool ignore_empty = false){
     std::vector<std::string> result;
@@ -27,12 +29,45 @@ std::vector<std::string> split(const std::string& s, const char delimiter, bool 
     return result;
 }
 
+void p_rekrytyt(vector<pair<string, string>> vektori, string henkilo, string pisteet);
+
+void print_alainen(vector<pair<string, string>> vektori, string henkilo,
+                    vector<pair<string, string>>::iterator it, string pisteet) {
+    pisteet += "..";
+    if(it->first == henkilo) {
+        cout << pisteet << it->second << endl;
+        henkilo = it->second;
+        p_rekrytyt(vektori, henkilo, pisteet);
+    }
+}
+
+void p_rekrytyt(vector<pair<string, string>> vektori, string henkilo, string pisteet) {
+    vector<pair<string, string>>::iterator it;
+    for(it = vektori.begin(); it != vektori.end(); ++it) {
+        print_alainen(vektori, henkilo, it, pisteet);
+    }
+}
+
+unsigned int kerro_verkoston_suuruus(vector<pair<string, string>> vektori, string henkilo) {
+    vector<pair<string, string>>::iterator it;
+    unsigned int verkoston_koko = 0;
+    for(it = vektori.begin(); it != vektori.end(); ++it) {
+        if(it->first == henkilo) {
+            ++verkoston_koko;
+            verkoston_koko += kerro_verkoston_suuruus(vektori, it->second);
+        }
+    }
+    return verkoston_koko;
+}
+
+
 
 
 int main()
 {
     // TODO: Implement the datastructure here
-
+    // Vektorissa pareja(rekryyjä, rekrytty)
+    std::vector<std::pair<std::string, std::string>> vektori;
 
     while(true){
         std::string line;
@@ -51,6 +86,7 @@ int main()
             std::string id2 = parts.at(2);
 
             // TODO: Implement the command here!
+            vektori.push_back(make_pair(id1, id2));
 
         } else if(command == "P" or command == "p"){
             if(parts.size() != 2){
@@ -60,6 +96,8 @@ int main()
             std::string id = parts.at(1);
 
             // TODO: Implement the command here!
+            string pisteet = "";
+            p_rekrytyt(vektori, id, pisteet);
 
         } else if(command == "C" or command == "c"){
             if(parts.size() != 2){
@@ -69,6 +107,7 @@ int main()
             std::string id = parts.at(1);
 
             // TODO: Implement the command here!
+            cout << kerro_verkoston_suuruus(vektori, id) << endl;
 
         } else if(command == "D" or command == "d"){
             if(parts.size() != 2){
