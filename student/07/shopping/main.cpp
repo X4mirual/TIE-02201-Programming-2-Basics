67849e92d9e2d12554f97103cc590d9d69fcb89d
Lauri Hiltunen <lauri.hiltunen@tuni.fi>
2020-03-09 14:38:01 +0200

    lisätty kommentit, laitettu jokainen koodirivi max 80 merkin pituiseksi

--- student/07/shopping/main.cpp
+++ student/07/shopping/main.cpp
@@ -1,21 +1,22 @@
 /* Kauppaketjut
  *
  * Kuvaus:
- * Ohjelma lukee tiedostosta kauppaketju-, (liikkeen) nimi-, tuote- ja
+ * Ohjelma lukee tiedostosta kauppaketju-, lokaatio-, tuote- ja
  * hintatiedot. Ohjelma kertoo tämän jälkeen sille annettujen komentojen
  * mukaan tiedoista.
  *
- * Program author
- * Name: Lauri Hiltunen
- * Student number: 274422
- * UserID: hiltunen
- * E-Mail: lauri.hiltune@tuni.fi
+ * Ohjelman tekijä
+ * Nimi: Lauri Hiltunen
+ * Opiskelijanumero: 274422
+ * Käyttäjätunnus: hiltunen
+ * Sähköpostiosoite: lauri.hiltune@tuni.fi
  *
  * Huomioitavaa ohjelmassa ja sen toteutuksessa:
  * Tiedot tiedostossa oltava muodossa
- * "kauppaketju;nimi;tuote;hinta" ja jokaisen tietoalkion oltava omalla
- * rivillään. Antaessa moniosaisia komentoja (kuten kysyessä ketjuun kuuluvia
- * liikkeitä) toimii erottimena välilyönti ' '.
+ * "kauppaketju;lokaatio;tuote;hinta" ja jokaisen tietoalkion oltava omalla
+ * rivillään. Tyhjiä tietokenttiä ei voi olla syötetiedostossa
+ * (esim ";;Hervanta;peruna;3" ei käy).
+ * Antaessa moniosaisia komentoja toimii erottimena välilyönti ' '.
  *
  * */
 
@@ -45,19 +46,17 @@ struct Product{
 map<string, map<string, vector<Product>>> chains;
 vector<string> product_names;
 
-//Jakaa merkkijono osiin
+//Jakaa merkkijono osiin.
 //Parametrit: jaettava merkkijono, osien välinen erotin
 //Paluuarvo: merkkijono jaettuna osiin
-std::vector<std::string> split(const std::string& s, const char delimiter){
+std::vector<std::string> split(const std::string& s, const char delimiter) {
     std::vector<std::string> result;
     std::string tmp = s;
 
-    while(tmp.find(delimiter) != std::string::npos)
-    {
+    while(tmp.find(delimiter) != std::string::npos) {
         std::string new_part = tmp.substr(0, tmp.find(delimiter));
         tmp = tmp.substr(tmp.find(delimiter)+1, tmp.size());
-        if(not(new_part.empty()))
-        {
+        if(new_part.empty() == false) {
             result.push_back(new_part);
         }
     }
@@ -65,50 +64,65 @@ std::vector<std::string> split(const std::string& s, const char delimiter){
     return result;
 }
 
-//Lisää tuotteen, tarvittaessa kauppaketjun lokaation ja tarvittaessa
-//kauppaketjun tietorakenteeseen
-//Parametrit:
-//Paluuarvo:
-void add_to_data_structure(string chain, string location, string product, string price_information) {
+//Lisää tuotteen, tarvittaessa kauppaketjuun lokaation ja tarvittaessa
+//kauppaketjun tietorakenteeseen.
+//Parametrit: kauppaketju, lokaatio, lisättävä tuote, tuotteen hinta tai
+//tieto ettei tuotetta ole varastossa
+//Paluuarvo: ei paluuarvoa
+void add_to_data_structure(string chain, string location, string product,
+                           string price_information) {
     double price;
     map<string, vector<Product>> temp;
+
+    //Jos tuote on loppuunmyyty, sen hinnaksi asetetaan maksimiluku
     if(price_information == "out-of-stock") {
         price = numeric_limits<int>::max();
     }
     else {
         price = stod(price_information);
     }
-    if(chains.find(chain) == chains.end()) { //chain not found
+    //chain not found
+    if(chains.find(chain) == chains.end()) {
         temp = {{location, {{product, price}}}};
         chains.insert(make_pair(chain, temp));
     }
-    else{ //chain found
-        if(chains.at(chain).find(location) == chains.at(chain).end()) { //chain not found, location not found
+    //chain found
+    else{
+        //chain not found, location not found
+        if(chains.at(chain).find(location) == chains.at(chain).end()) {
             chains.at(chain).insert({{location, {{product, price}}}});
         }
-        else { //chain found, location found
-            int i = 0;
-                for(vector<Product>::iterator it = chains.at(chain).at(location).begin();
-                it != chains.at(chain).at(location).end(); ++it) {
-                    if(it->product_name == product) { //chain found, location found, product found
+        //chain found, location found
+        else {
+            bool product_already_in_structure = false;
+                for(vector<Product>::iterator
+                        it = chains.at(chain).at(location).begin();
+                        it != chains.at(chain).at(location).end(); ++it) {
+                    //chain found, location found, product found
+                    if(it->product_name == product) {
                         it->price = price;
-                        i = 1;
+                        product_already_in_structure = true;
                     }
                 }
-            if(i == 0) { // chain found, location found, product not found
+            // chain found, location found, product not found
+            if(product_already_in_structure == false) {
                 chains.at(chain).at(location).push_back({product, price});
-            }
-            else {
-                i = 0;
             }
         }
     }
-    if(find(product_names.begin(), product_names.end(), product) == product_names.end()) { //product not found in list
+    // If product not in list of product names, it is added
+    if(find(product_names.begin(), product_names.end(), product) ==
+            product_names.end()) {
         product_names.push_back(product);
     }
 }
 
-
+//Kysyy käyttäjältä luettavan tiedoston ja lukee sen rivi riviltä lisäten
+//funktion "add_to_data_structure" avulla tiedot tietorakenteeseen
+//Parametrit: ei parametreja
+//Paluuarvo: tiedoston lukemisen onnistuminen (tämän perusteella päätetään
+//ohjelman suorittamisen jatkamisesta). Paluuarvo false palautetaan
+//välittömästi, jos rivin lukeminen tiedostosta ei onnistu.
 bool read_file() {
     string file_name;
     cout << "Input file: ";
@@ -137,19 +151,33 @@ bool read_file() {
     return true;
 }
 
-//Tee lista
+//Tulostaa halvimman hinnan kysytylle tuotteelle sekä kauppaketjun ja
+//lokaation, joista tuotetta on saatavissa tuohon hintaan.
+//Parametrit: tuote (jolle halutaan halvin hinta ja vastaavat myyntipaikat)
+//Paluuarvo: ei paluuarvoa
 void print_cheapest_places(string product) {
     vector<string> cheapest_places;
     double cheapest_price_so_far = numeric_limits<double>::infinity();
     bool is_out_of_stock = false;
 
     //Käydään läpi tietorakenteessa oleva jokainen tuote, merkaten muistiin
-    //halvimpien sijainnit (liikkeet) ja hinta
-    for(map<string, map<string, vector<Product>>>::iterator it1 = chains.begin(); it1 != chains.end(); ++it1) {
-        for(map<string, vector<Product>>::iterator it2 = chains.at(it1->first).begin(); it2 != chains.at(it1->first).end(); ++it2){
-            for(vector<Product>::iterator it3 = chains.at(it1->first).at(it2->first).begin(); it3 != chains.at(it1->first).at(it2->first).end(); ++it3) {
+    //kysytyn tuotteen halvin hinta ja sitä vastaavat lokaatiot ja niiden
+    //kauppaketjut
+    //Jokainen kauppaketju
+    for(map<string, map<string, vector<Product>>>::iterator it1 =
+            chains.begin(); it1 != chains.end(); ++it1) {
+        //Kauppaketjun jokainen lokaatio
+        for(map<string, vector<Product>>::iterator it2 =
+                chains.at(it1->first).begin(); it2 !=
+                chains.at(it1->first).end(); ++it2){
+            //Lokaation jokainen tuote
+            for(vector<Product>::iterator it3 =
+                    chains.at(it1->first).at(it2->first).begin(); it3 !=
+                    chains.at(it1->first).at(it2->first).end(); ++it3) {
                 string temp;
                 if(it3->product_name == product) {
+                    //Maksimiluku hintana tarkoittaa, että tuotetta ei ole
+                    //varastossa.
                     if(it3->price == numeric_limits<int>::max()) {
                         is_out_of_stock = true;
                     }
@@ -167,8 +195,8 @@ void print_cheapest_places(string product) {
             }
         }
     }
+    //Tulostetaan tulokset.
 
-    //Tulostetaan tulokset
     if(cheapest_price_so_far != numeric_limits<double>::infinity()) {
         cout << cheapest_price_so_far << " euros" << endl;
         for(string individual : cheapest_places) {
@@ -183,10 +211,18 @@ void print_cheapest_places(string product) {
     }
 }
 
+//Vertaa tuotenimien järjestystä, käytetään tuotteiden järjestelemiseen
+//tuotenimien mukaan aakkosjärjestykseen.
+//Parametrit: (stucteina) tuotteet, jotka halutaan järjestää nimensä mukaan
+//Paluuarvo: tieto kahden tuotenimen aakkosjärjestyksestä
 bool compare_product_names(Product a, Product b) {
     return a.product_name < b.product_name;
 }
 
+//Tulostaa tietyn ketjun tietyn lokaation tuotteet ja niiden hinnat (tai
+//tiedon tuotteen olemisesta loppu).
+//Parametrit: kauppaketju, lokaatio (jonka tuotevalikoima halutaan tulostaa)
+//Paluuarvo: ei paluuarvoa
 void print_selection(string chain, string location) {
     if(chains.find(chain) == chains.end()) {
         cout << "Error: unknown chain name" << endl;
@@ -197,8 +233,9 @@ void print_selection(string chain, string location) {
     else {
         sort(chains.at(chain).at(location).begin(),
              chains.at(chain).at(location).end(), compare_product_names);
-        for(vector<Product>::iterator it = chains.at(chain).at(location).begin();
-        it != chains.at(chain).at(location).end(); ++it) {
+        for(vector<Product>::iterator it =
+                chains.at(chain).at(location).begin();
+                it != chains.at(chain).at(location).end(); ++it) {
             if(it->price != numeric_limits<int>::max()) {
                 cout << it->product_name << " " << it->price << endl;
             }
@@ -209,7 +246,12 @@ void print_selection(string chain, string location) {
     }
 }
 
-bool is_command_length(vector<string> command_parts, int right_length) {
+//Tarkistaa, että annetussa komennossa on oikea lukumäärä komponentteja.
+//Parametrit: komento vektorina (jossa komennon komponentit vektorissa
+//omina alkioinaan)
+//Paluuarvo: oliko komennossa oikea lukumäärä komponentteja vai ei
+bool is_command_length(vector<string> command_parts,
+                       unsigned int right_length) {
     if(command_parts.size() != right_length) {
         cout << "Error: error in command " << command_parts.at(0) << endl;
         return false;
@@ -226,16 +268,21 @@ int main() {
     //Asetetaan tarkkuus kaikille lukujen tulostuksille
     cout << std::fixed << setprecision(2);
 
+    //Järjestetään tuotenimien luettelo, mahdollista tulostusta
+    //products-komennolla varten
     sort(product_names.begin(), product_names.end());
     string command;
     vector<string> command_parts;
+
     while(command != "quit") {
         cout << "> ";
         getline(cin, command);
         command_parts = split(command, ' ');
+
         if(command_parts.at(0) == "chains") {
             if(is_command_length(command_parts, 1) == true) {
-                for(map<string, map<string, vector<Product>>>::iterator it = chains.begin(); it != chains.end(); ++it) {
+                for(map<string, map<string, vector<Product>>>::iterator it =
+                        chains.begin(); it != chains.end(); ++it) {
                     cout << it->first << endl;
                 }
             }
@@ -247,7 +294,8 @@ int main() {
                     cout << "Error: unknown chain name" << endl;
                 }
                 else {
-                    for(auto it = chains.at(location).begin(); it != chains.at(location).end(); ++it) {
+                    for(auto it = chains.at(location).begin(); it !=
+                            chains.at(location).end(); ++it) {
                         cout << it->first << endl;
                     }
                 }
@@ -260,8 +308,7 @@ int main() {
         }
         else if(command_parts.at(0) == "cheapest") {
             if(is_command_length(command_parts, 2) == true) {
-                string product = command_parts.at(1);
-                print_cheapest_places(product);
+                print_cheapest_places(command_parts.at(1));
             }
         }
         else if(command == "products") {
@@ -273,7 +320,8 @@ int main() {
         }
         else if(command != "quit") {
             if(is_command_length(command_parts, 1) == true) {
-                cout << "Error: unknown command: " << command_parts.at(0) << endl;
+                cout << "Error: unknown command: " << command_parts.at(0)
+                     << endl;
             }
         }
     }
