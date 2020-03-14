19e41cc897cf8cbb228cf975e797fc997ecbea5e
Lauri Hiltunen <lauri.hiltunen@tuni.fi>
2020-03-14 19:02:43 +0200

    ou jeeee

--- student/08/reverse_polish/main.cpp
+++ student/08/reverse_polish/main.cpp
@@ -2,41 +2,95 @@
 
 using namespace std;
 
+bool is_too_few_operands(int index) {
+    if (index == 1) {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
 int main()
 {
     cout << "Input an expression in reverse Polish notation (end with #):" << endl;
     cout << "EXPR> ";
     char c;
     int N = 80;
-    int u;
-    char merkit[N];
-    for(int i = 0; i < N; ++i) {
+    int index = 0;
+    int merkit[N];
+    bool failure = false;
+    while(true) {
         cin >> c;
         if(c == ' ') {
             continue;
         }
-        merkit[i] = c;
 
-        if(c == '#') {
-            u = i;
+        if(c =='+' || c =='-' || c =='*' || c =='/') {
+            if(is_too_few_operands(index) == true) {
+                cout << "Error: Too few operands" << endl;
+                failure = true;
+                break;
+            }
+            else if(index == 0) {
+                cout << "Error: Expression must start with a number"
+                     << endl;
+                failure = true;
+                break;
+            }
+
+            else if(c == '+') {
+                merkit[index - 2] = merkit[index - 2] + merkit[index - 1];
+                merkit[index - 1] = 0;
+                index -= 2;
+            }
+            else if(c == '-') {
+                merkit[index - 2] = merkit[index - 2] - merkit[index - 1];
+                merkit[index - 1] = 0;
+                index -= 2;
+            }
+            else if(c == '*') {
+                merkit[index - 2] = merkit[index - 2] * merkit[index - 1];
+                merkit[index - 1] = 0;
+                index -= 2;
+            }
+            else if(c == '/') {
+                if(merkit[index - 1] == 0) {
+                    cout << "Error: Division by zero" << endl;
+                    failure = true;
+                    break;
+                }
+                merkit[index - 2] = merkit[index - 2] / merkit[index - 1];
+                merkit[index - 1] = 0;
+                index -= 2;
+            }
+        }
+        else if(c == '#') {
             break;
         }
+        else if(c < 48 || c > 59) {
+            cout << "Error: Unknown character" << endl;
+            failure = true;
+            break;
+        }
+        else {
+            merkit[index] = c - 48;
+        }
+        ++index;
     }
-
-    for(int i = 0; i < u; ++i) {
-        cout << merkit[i];
+    if(failure == true) {
+        return 1;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-    return 0;
+    if(merkit[1] != 0) {
+        cout << "Error: Too few operators" << endl;
+        failure = true;
+    }
+    if(failure == true) {
+        return 1;
+    }
+    else{
+        cout << "Correct: " << merkit[0]
+             << " is the result" << endl;
+        return 0;
+    }
 }
