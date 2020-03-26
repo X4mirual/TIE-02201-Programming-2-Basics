c6066b4ddd6128e54aff0583f53a94d219cdbb9c
Lauri Hiltunen <lauri.hiltunen@tuni.fi>
2020-03-26 18:52:56 +0200

    jeesbox

--- student/10/reverse/cards.cpp
+++ student/10/reverse/cards.cpp
@@ -2,6 +2,8 @@
 #include <memory>
 #include "cards.hh"
 
+using namespace std;
+
 
 Cards::Cards(): top_( nullptr ) {
 }
@@ -24,7 +26,75 @@ void Cards::print(std::ostream& s) {
    }
 }
 
+// Removes the topmost card and passes it in the reference parameter id.
+// Returns false, if the data structure is empty, otherwise returns true.
+bool Cards::remove(int &id) {
+    if(top_ == nullptr) {
+        return false;
+    }
+    else {
+        shared_ptr<Card_data> removable_ptr = top_;
+        id = removable_ptr->data;
+        top_ = top_->next;
+        return true;
+    }
+}
+
+// Reverses the content of the data structure as opposite.
+void Cards::reverse() {
+    shared_ptr<Card_data> bottom = top_;
+    int i = 0;
+    while ( bottom->next != nullptr ) {
+       bottom = bottom->next;
+       ++i;
+    }
+
+
+
+    shared_ptr<Card_data> chosen_card = top_;
+    shared_ptr<Card_data> prev_chosen;
+   /* while(chosen_card->next != nullptr) {
+        prev_chosen = chosen_card;
+        chosen_card = chosen_card->next;
+        top_ = chosen_card;
+}*/
+    //card values: 1,2,3,4,5
+    //chosen: 5
+    chosen_card = chosen_card->next; //chosen: 4
+    int n = 1;
+    while(chosen_card->next != nullptr) {
+        prev_chosen = chosen_card; //prev_chosen = 4
+        chosen_card = chosen_card->next; //chosen_card = 3
+        //top_ = 5
+        prev_chosen->next = top_;
+        if(n == 1) {
+            top_->next = nullptr;
+            n = 0;
+        }
+        top_ = prev_chosen;
+    }
+    chosen_card->next = top_;
+    top_ = chosen_card;
+}
+
 // Do not write the stubs of the methods remove and reverse by yourself here,
 // but open the file reverse.hh and click the declaration of the method
 // by the right mouse button and select
 // Refactor > Add definition in reverse.cpp
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
