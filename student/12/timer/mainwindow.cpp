d0da7cd86da86201a1c3e457e92727b88ccb8ae3
Lauri Hiltunen <lauri.hiltunen@tuni.fi>
2020-04-23 13:22:42 +0300

    ou jeee B)

--- student/12/timer/mainwindow.cpp
+++ student/12/timer/mainwindow.cpp
@@ -1,14 +1,64 @@
 #include "mainwindow.hh"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    timer = new QTimer;
+    timer->setInterval(1000);
+    connect(timer, SIGNAL(timeout()), this, SLOT(on_timeChange()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_startButton_clicked()
+{
+    if(not timer->isActive()) {
+        timer->start();
+    }
+}
+
+void MainWindow::on_stopButton_clicked()
+{
+    if(timer->isActive()) {
+        timer->stop();
+    }
+}
+
+void MainWindow::on_timeChange()
+{
+    ++seconds_;
+    updateDisplay();
+}
+
+void MainWindow::on_resetButton_clicked()
+{
+    on_stopButton_clicked();
+    seconds_ = 0;
+    updateDisplay();
+}
+
+void MainWindow::updateDisplay()
+{
+    ui->lcdNumberSec->display(seconds_%60);
+    ui->lcdNumberMin->display(seconds_/60);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- student/12/timer/mainwindow.hh
+++ student/12/timer/mainwindow.hh
@@ -24,9 +24,20 @@ public:
 private slots:
     // Add your slots here
 
+    void on_startButton_clicked();
+
+    void on_stopButton_clicked();
+
+    void on_timeChange();
+
+    void on_resetButton_clicked();
+
 private:
+    void updateDisplay();
+
     Ui::MainWindow *ui;
     // Add other necessary members here
+    int seconds_ = 0;
 };
 
 #endif // MAINWINDOW_HH
